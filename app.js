e27c1c2a6b69dd9a232ab40c1858a675e509c714
TS22082 <ts22082@gmail.com>
2020-05-28 15:50:05 -0700

    convert to async/await for resolving promises

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const colors = require("colors");
 
-(function async() {
-  axios.get("https://arstechnica.com/gadgets/").then((response) => {
-    const $ = cheerio.load(response.data);
+(async function () {
+  try {
+    const { data } = await axios.get("https://arstechnica.com/gadgets/");
+    const $ = cheerio.load(data);
 
     $("li.article").each((i, element) => {
       const link = $(element).find("a.overlay").attr("href");
-
       const header = $(element).find("a").text().split("   ")[0];
 
       console.log(header.yellow);
       console.log(link.cyan);
       console.log("---------------------------\n");
     });
-  });
+  } catch (err) {
+    console.log({ error: err });
+  }
 })();
