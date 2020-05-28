d238b5bc95b54fbc2882ec331f0a357c67fa2e83
TS22082 <ts22082@gmail.com>
2020-05-28 15:45:35 -0700

    nest in IEFE

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const colors = require("colors");
 
-axios.get("https://arstechnica.com/gadgets/").then(response => {
-  const $ = cheerio.load(response.data);
+(function async() {
+  axios.get("https://arstechnica.com/gadgets/").then((response) => {
+    const $ = cheerio.load(response.data);
 
-  $("li.article").each((i, element) => {
-    const link = $(element)
-      .find("a.overlay")
-      .attr("href");
+    $("li.article").each((i, element) => {
+      const link = $(element).find("a.overlay").attr("href");
 
-    const header = $(element)
-      .find("a")
-      .text()
-      .split("   ")[0];
+      const header = $(element).find("a").text().split("   ")[0];
 
-    console.log(header.yellow);
-    console.log(link.cyan);
-    console.log("---------------------------\n");
+      console.log(header.yellow);
+      console.log(link.cyan);
+      console.log("---------------------------\n");
+    });
   });
-});
+})();
